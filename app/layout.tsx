8dd3bdddd6d948ba0d44c1ed5d150e03e446edce
Saksham Kumar <saksham.kumar.ug22@nsut.ac.in>
2025-08-24 23:25:33 +0530

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,55 @@
+import { ReactNode } from 'react';
+import type { Metadata } from 'next';
+import { ClerkProvider } from '@clerk/nextjs';
+import { Inter, Space_Grotesk } from 'next/font/google';
+
+import './globals.css';
+import '@stream-io/video-react-sdk/dist/css/styles.css';
+import 'react-datepicker/dist/react-datepicker.css';
+import { Toaster } from '@/components/ui/toaster';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'VirtuMeet',
+  description: 'Video calling App',
+  icons: {
+    icon: '/icons/logo.svg',
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  return (
+    <ClerkProvider
+      appearance={{
+        layout: {
+          socialButtonsVariant: 'iconButton',
+          logoImageUrl: '/icons/virtumeet-logo.svg',
+        },
+        variables: {
+          colorText: '#fff',
+          colorPrimary: '#0E78F9',
+          colorBackground: '#1C1F2E',
+          colorInputBackground: '#252A41',
+          colorInputText: '#fff',
+        },
+        elements: {
+          logoBox: {
+            display: 'none',
+          },
+        },
+      }}
+    >
+      <html lang="en">
+        <body className={`${inter.className} bg-dark-2`}>
+          <Toaster />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
